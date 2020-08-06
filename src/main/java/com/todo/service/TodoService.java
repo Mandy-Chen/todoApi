80d9dd6020bcf95397be344c70c33fe3d494ff0a
497133212 <497133212@qq.com>
2020-08-06 11:37:24 +0800

    [todoApi][mandy] feature :implement add todo

--- src/main/java/com/todo/service/TodoService.java
+++ src/main/java/com/todo/service/TodoService.java
@@ -1,9 +1,23 @@
 package com.todo.service;
 
 import com.todo.model.Todo;
+import com.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
+@Service
 public class TodoService {
+    @Autowired
+    TodoRepository todoRepository;
+
     public Todo addTodo(Todo todo) {
-        return null;
+        if (Objects.isNull(todo)) {
+            System.out.println("todo为空！");
+        }
+        return todoRepository.save(todo);
+
     }
+
 }
